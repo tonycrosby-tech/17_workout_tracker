9a14408a969bcf46fcfcc40c97c55e10cd5f7c1e
tony913 <tonycrosby96@gmail.com>
2021-01-23 16:39:44 -0600

    deploying

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
